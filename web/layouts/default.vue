981bd6865347a620c5d000873131b2bc3402fc94
Dani Prasetya <dani.prasetya.dev@gmail.com>
2021-08-13 21:53:43 +0700

    [F] layouting #2

--- web/layouts/default.vue
+++ web/layouts/default.vue
@@ -22,24 +22,6 @@
     <v-main class="grey lighten-3">
       <v-container fluid> <nuxt /> </v-container
     ></v-main>
-
-    <v-footer v-bind="localAttrs" :padless="padless">
-      <v-card flat tile width="100%" class="red lighten-1 text-center">
-        <v-card-text>
-          <v-btn v-for="icon in icons" :key="icon" class="mx-4" icon>
-            <v-icon size="24px">
-              {{ icon }}
-            </v-icon>
-          </v-btn>
-        </v-card-text>
-
-        <v-divider></v-divider>
-
-        <v-card-text class="white--text">
-          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-        </v-card-text>
-      </v-card>
-    </v-footer>
   </v-app>
 </template>
 
--- web/pages/aboutme/index.vue
+++ web/pages/aboutme/index.vue
@@ -1,13 +1,8 @@
 <template>
-  <v-container class="fill-height">
+  <v-container>
     <v-row>
-      <v-col cols="12" sm="3">
-        <v-card
-          :loading="loading"
-          class="mx-auto rounded-0"
-          max-width="374"
-          height="100%"
-        >
+      <v-col cols="12" sm="3" class="m-0">
+        <v-card class="mx-auto rounded-0">
           <template slot="progress">
             <v-progress-linear
               color="deep-purple"
@@ -18,7 +13,8 @@
 
           <v-img
             height="250"
-            src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+            src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg"
+            gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)"
           ></v-img>
 
           <v-card-title>Dani Prasetya</v-card-title>
@@ -29,6 +25,34 @@
 
             <div>God damn person with kidness.</div>
           </v-card-text>
+          <v-divider class="mx-4"></v-divider>
+
+          <v-card-title>Education</v-card-title>
+          <v-card-actions>
+            <v-timeline dense class="mx-1">
+              <v-timeline-item small fill-dot>
+                <div>
+                  <h3
+                    :class="`headline text-subtitle-1 font-weight-light mb-2 blue--text`"
+                  >
+                    Electronic Engineering Polytechnic Institute of Surabaya
+                    (EEPIS) - 2016
+                  </h3>
+                  <div>B.App.Sc - Computer Science</div>
+                </div>
+              </v-timeline-item>
+              <v-timeline-item small fill-dot>
+                <div>
+                  <h3
+                    :class="`headline text-subtitle-1 font-weight-light mb-2 blue--text`"
+                  >
+                    Politeknik Negeri Jember - 2012
+                  </h3>
+                  <div>D3 - Associate Degree of computer science</div>
+                </div>
+              </v-timeline-item>
+            </v-timeline>
+          </v-card-actions>
 
           <v-divider class="mx-4"></v-divider>
 
@@ -44,8 +68,78 @@
       </v-col>
 
       <v-col cols="12" sm="9">
-        <v-sheet height="100%">
-          <!--  -->
+        <v-sheet class="pa-6">
+          <h2>Nobody ...</h2>
+          <v-divider class="mb-2"></v-divider>
+          <body-2>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </body-2>
+
+          <h2 class="mt-4">Tech Stack</h2>
+          <v-divider class="mb-2"></v-divider>
+          <v-row>
+            <v-col cols="6" sm="4">
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg"
+                gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)"
+              ></v-img>
+            </v-col>
+
+            <v-col cols="6" sm="4">
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg"
+              >
+                <div class="fill-height bottom-gradient"></div>
+              </v-img>
+            </v-col>
+
+            <v-col cols="6" sm="4">
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg"
+              >
+                <div class="fill-height repeating-gradient"></div>
+              </v-img>
+            </v-col>
+          </v-row>
+
+          <h2 class="mt-4">Experience</h2>
+          <v-divider class="mb-2"></v-divider>
+          <v-timeline>
+            <v-timeline-item
+              v-for="(year, i) in years"
+              :key="i"
+              :color="year.color"
+              small
+            >
+              <template v-slot:opposite>
+                <span
+                  :class="`headline font-weight-bold ${year.color}--text`"
+                  v-text="year.year"
+                ></span>
+              </template>
+              <div class="py-4">
+                <h2
+                  :class="`headline font-weight-light mb-4 ${year.color}--text`"
+                >
+                  Lorem ipsum
+                </h2>
+                <div>
+                  Lorem ipsum dolor sit amet, no nam oblique veritus. Commune
+                  scaevola imperdiet nec ut, sed euismod convenire principes at.
+                  Est et nobis iisque percipit, an vim zril disputando
+                  voluptatibus, vix an salutandi sententiae.
+                </div>
+              </div>
+            </v-timeline-item>
+          </v-timeline>
         </v-sheet>
       </v-col>
     </v-row>
@@ -56,6 +150,29 @@ export default {
   name: 'AboutMe',
   components: {},
   layout: 'default',
-  setup() {},
+  data: () => ({
+    years: [
+      {
+        color: 'cyan',
+        year: '1960',
+      },
+      {
+        color: 'green',
+        year: '1970',
+      },
+      {
+        color: 'pink',
+        year: '1980',
+      },
+      {
+        color: 'amber',
+        year: '1990',
+      },
+      {
+        color: 'orange',
+        year: '2000',
+      },
+    ],
+  }),
 }
 </script>
