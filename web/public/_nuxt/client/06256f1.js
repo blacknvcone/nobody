b0e0feff7d0e4372f04ebd5172df7d334dd4c452
Dani Prasetya <fachrul.prasetya@kanosolution.com>
2021-08-19 15:30:58 +0700

    testing deployment at firebase

---
+++ web/public/_nuxt/client/06256f1.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{465:function(n,t,e){"use strict";e.r(t);var o={name:"Projects",components:{},layout:"default",data:function(){return{}},mounted:function(){},methods:{}},c=e(86),component=Object(c.a)(o,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);t.default=component.exports}}]);
---
+++ web/public/_nuxt/client/d6e6806.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{466:function(n,t,e){"use strict";e.r(t);var o={components:{},layout:"default",data:function(){return{}},mounted:function(){},methods:{}},c=e(86),component=Object(c.a)(o,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);t.default=component.exports}}]);
